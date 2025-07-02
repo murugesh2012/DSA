89ac9f1c35cd8d20864fb917928c65ec7e5de86d
murugesh2012 <smurugesh1686@gmail.com>
2025-07-02 16:25:24 +0530

    changed logic

--- src/sortingalgos/BubbleSort.java
+++ src/sortingalgos/BubbleSort.java
@@ -13,13 +13,18 @@ public class BubbleSort {
         for (int i = 0; i < numberOfElements; i++) {
             arr[i] = sc.nextInt();
         }
+        boolean swapped=false;
         for (int i = 0; i < numberOfElements; i++) {
             for (int j = 1; j < numberOfElements-i; j++) {
                 if (arr[j-1] > arr[j]) {
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
+                    swapped=true;
                 }
+            }
+            if(!swapped){
+                break;
             }
         }
         for (int num : arr) {
